4e92b253e0c1f5d58b5f581989c202341cc8c084
ArfySaysHi <necroticowl@gmail.com>
2026-05-09 12:03:56 +0100

    qol: added convenient logging for linux builds and removed some more frameless window includes within cmake

--- cmake/linux_postfind.cmake
+++ cmake/linux_postfind.cmake
@@ -5,3 +5,9 @@ if( NOGGIT_ALL_WARNINGS )
   MESSAGE( STATUS "Spilling out mass warnings." )
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Wshadow")
 endif( NOGGIT_ALL_WARNINGS )
+
+# On Linux, default to console logging for easier debugging
+if(NOT DEFINED NOGGIT_LOGTOCONSOLE)
+  set(NOGGIT_LOGTOCONSOLE ON CACHE BOOL "Log to console instead of log.txt" FORCE)
+  add_definitions(-DDEBUG__LOGGINGTOCONSOLE)
+endif()
--- cmake/linux_prefind.cmake
+++ cmake/linux_prefind.cmake
@@ -1,3 +1,4 @@
 # This file is part of Noggit3, licensed under GNU General Public License (version 3).
 
+list(APPEND CMAKE_PREFIX_PATH /usr/lib/qt5)
 SET(Boost_USE_STATIC_LIBS OFF)
